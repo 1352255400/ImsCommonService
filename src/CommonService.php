f8078724c8c8ef1105a0cf2f63f6f29fbbbddd59
W_wang <1352255400@qq.com>
2019-08-06 11:23:08 +0800

    V1.0.0

---
+++ src/CommonService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ImsCommonService;
+
+// use think\facade\Cache;
+
+
+/**
+ * [TpCacheService tp缓存 Logic]
+ * @Author   W_wang
+ * @email    1352255400@qq.com
+ * @DateTime 2018-04-17T13:51:51+0800
+ */
+class CommonService
+{
+    public static function d()
+    {
+        echo 11;
+    }
+}
