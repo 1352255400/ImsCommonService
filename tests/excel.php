d51791a0cc3e5ac69c1a3a54138d8b6feb1f15c3
W_wang <1352255400@qq.com>
2019-12-11 16:10:53 +0800

    注释修改

--- tests/excel.php
+++ tests/excel.php
@@ -32,15 +32,15 @@ if (!empty($type)) {
     $expTableData = [];
     $expTableData[] = [
         'name' => '测试1',//sheet名称
-        'title' => ['标题1', '标题2', '标题3'], //标题
-        'data' => [['a1', 'b1', 'c1'], ['aa1', 'bb1', 'cc1']] //内容
+        'title' => ['标题1', '标题2', '标题3'], //表头
+        'data' => [['a1', 'b1', 'c1'], ['aa1', 'bb1', 'cc1']], //内容
+        'color' => 'FFCC0001',//字体颜色
+        'color_row' => [['row' => 1, 'col_num' => 1], ['row' => 3, 'col_num' => 2]] //row第几行，col_num列数
     ];
     $expTableData[] = [
         'name' => '测试2',
         'title' => ['标题11', '标题22', '标题33'],
         'data' => [['a2', 'b2', 'c2'], ['aa2', 'bb2', 'cc2']],
-        'color' => 'FFCC0001',//字体颜色
-        'color_row' => [['row' => 1, 'col_num' => 1], ['row' => 3, 'col_num' => 2]] //row第几行，col_num列数
     ];
     if ($type == 1) {
         $re = $excle->export('demo', $expTableData);
